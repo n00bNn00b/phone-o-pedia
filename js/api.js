c56e3bc4c844c40057a93d952bf1adaa1ee91a83
n00bNn00b <realdewan9@gmail.com>
2022-03-02 21:40:21 +0600

    20 results of phone display along with error handling excluding phone details

--- js/api.js
+++ js/api.js
@@ -1,1 +1,52 @@
-console.log("Working!");
+// spinner
+const loadingSpinner = (displayStyle) => {
+  document.getElementById("spinner").style.display = displayStyle;
+};
+
+// search box value
+const searchPhone = () => {
+  const inputText = document.getElementById("search-box");
+  const inputValue = inputText.value;
+  const error = document.getElementById("error");
+  loadingSpinner("block");
+  if (inputValue == "") {
+    error.style.display = "block";
+    loadingSpinner("none");
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputValue}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayPhones(data.data));
+
+    error.style.display = "none";
+    loadingSpinner("none");
+  }
+};
+
+// display less than or equal to 20 phones as list
+const displayPhones = (data) => {
+  const phoneList = document.getElementById("phone-list");
+  phoneList.textContent = ""; // clearing previous result
+  //   empty string error handling
+  if (data.length == 0) {
+    error.style.display = "block";
+  } else {
+    const phoneResults = data.slice(0, 20);
+    phoneResults.forEach((phone) => {
+      console.log(phone);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+    <div class="card">
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      <p class="card-text">Brand: ${phone.brand}</p>
+      <a href="#phone-details"><button onclick="details('${phone.slug}')"class="rounded btn-primary">Click for Details</button></a>
+    </div>
+  </div>
+    `;
+      phoneList.appendChild(div);
+    });
+  }
+};
